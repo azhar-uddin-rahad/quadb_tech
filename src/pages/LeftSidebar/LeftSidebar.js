0315b45977f2989a5906ce1de578fe51250a62d5
azharrahad31 <azherudinrahad@gmail.com>
2022-12-17 01:20:40 +0600

    leftsidbar complite

--- src/pages/LeftSidebar/LeftSidebar.js
+++ src/pages/LeftSidebar/LeftSidebar.js
@@ -13,7 +13,30 @@ const LeftSidebar = () => {
 
 
     return (
-
+        <div>
+            <h3>All Category: {categories.length}</h3>
+            {
+                categories.map(categori => <div
+                key={categori.show.id}><Link class="text-decoration-none" to={`/category/${categori.show.id}`}> 
+<div class="card mb-3 h-25">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src={categori.show.image?.medium} class="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title text-dark">Card title</h5>
+        <p class="card-text text-dark">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text text-dark"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+</Link>
+                
+                
+                </div>)
+            }
         </div>
     );
 };
